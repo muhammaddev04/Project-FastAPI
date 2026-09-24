5aacf4a32cb2a5c0a82fd4dde554a8182f1af499
muhammaddev04 <muhammadmahmudzoda04@gmail.com>
2026-09-24 14:00:04 +0500

    style: add auth page feedback states

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import './styles/auth.css';
 import './styles/forms.css';
 import './styles/roles.css';
+import './styles/pages.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
